6fcb067b6cad162a142a50fc5ce098370b10129c
Bruce P Cox Jr <bpcoxjr@yahoo.com>
2016-04-24 10:28:45 -0400

    refactored API call

--- app/js/factories/api.js
+++ app/js/factories/api.js
@@ -1,13 +1,9 @@
 //factory function for calls to NYT API//
 
-var svc_search_v2_articlesearch = function(response){
-	console.log(response);
-};
-
 angular.module('ArticleServices', [])
 
 	.constant('key', 'da049a2ebfeed654a53bd38ab6573867:9:60954891')
-	.constant('baseUrl', '//api.nytimes.com/svc/search/v2/articlesearch')
+	.constant('baseUrl', '//api.nytimes.com/svc/search/v2/articlesearch.jsonp?')
 	.constant('callback', 'callback=svc_search_v2_articlesearch')
 
 	.factory('articleFactory', ['$http', 'key', 'baseUrl', 'callback', '$route', '$q', function($http, key, baseUrl, callback, $route, $q){
@@ -16,15 +12,11 @@ angular.module('ArticleServices', [])
 				query = query || {};
 				console.log(query);
 				var q = query;
-				return $http.get(baseUrl + '.' + 'jsonp?' + '[' + q + ']' + callback + '&' + 'api-key=' + key).then(function(result){
-					JSON.parse(result);
+				return $http.jsonp(baseUrl + 'q=' + q + '&' + callback + '&' + 'api-key=' + key).then(function(result){
+					JSONP.parse(result);
 					console.log(result);
 					return result.response;
 				});
 			}
 			return {getArticles: getArticles};	
 	}]);
-
-
-//expected API construction:
-//http://api.nytimes.com/svc/search/v2/articlesearch.jsonp?[query]callback=svc_search_v2_articlesearch&api-key=da049a2ebfeed654a53bd38ab6573867:9:60954891
