233326ef0d66724eba128686395d80ed7b229f16
Bruce P Cox Jr <bpcoxjr@yahoo.com>
2016-04-24 09:58:06 -0400

    refactored API call

--- _build/js/controllers.js
+++ _build/js/controllers.js
@@ -31,7 +31,7 @@ angular.module('timesSearchApp')
 		console.log('Returning to blank form!');
 	};
 }]);
-}).call(this,require("rH1JPG"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_a7538d1a.js","/")
+}).call(this,require("rH1JPG"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_957b55bc.js","/")
 },{"buffer":3,"rH1JPG":5}],2:[function(require,module,exports){
 (function (process,global,Buffer,__argument0,__argument1,__argument2,__argument3,__filename,__dirname){
 var lookup = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
--- app/js/factories/api.js
+++ app/js/factories/api.js
@@ -6,20 +6,17 @@ var svc_search_v2_articlesearch = function(response){
 
 angular.module('ArticleServices', [])
 
-	.constant('apiKey', 'da049a2ebfeed654a53bd38ab6573867:9:60954891')
-	.constant('url', '//api.nytimes.com/svc/search/v2/articlesearch')
-	.constant('callback', 'svc_search_v2_articlesearch')
+	.constant('key', 'da049a2ebfeed654a53bd38ab6573867:9:60954891')
+	.constant('baseUrl', '//api.nytimes.com/svc/search/v2/articlesearch')
+	.constant('callback', 'callback=svc_search_v2_articlesearch')
 
-	.factory('articleFactory', ['$http', 'apiKey', 'url', 'callback', '$route', '$q', function($http, apiKey, url, callback, $route, $q){
+	.factory('articleFactory', ['$http', 'key', 'baseUrl', 'callback', '$route', '$q', function($http, key, baseUrl, callback, $route, $q){
 
 			function getArticles(query){
 				query = query || {};
 				console.log(query);
-				var params = {'response-format': 'jsonp', 'api-key': apiKey, 'callback': callback, 'q': query}; //NYT API required format
-				
-
-				return $http.get(url, {params
-				}).then(function(result){
+				var q = query;
+				return $http.get(baseUrl + '.' + 'jsonp?' + '[' + q + ']' + callback + '&' + 'api-key=' + key).then(function(result){
 					JSON.parse(result);
 					console.log(result);
 					return result.response;
@@ -28,3 +25,6 @@ angular.module('ArticleServices', [])
 			return {getArticles: getArticles};	
 	}]);
 
+
+//expected API construction:
+//http://api.nytimes.com/svc/search/v2/articlesearch.jsonp?[query]callback=svc_search_v2_articlesearch&api-key=da049a2ebfeed654a53bd38ab6573867:9:60954891
