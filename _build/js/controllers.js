4c71746f53d342cbf9efd8211fabb9753be50be6
Bruce P Cox Jr <bpcoxjr@yahoo.com>
2016-04-29 22:35:00 -0400

    worked on sticky header

--- _build/js/controllers.js
+++ _build/js/controllers.js
@@ -98,7 +98,7 @@ angular.module('timesSearchApp')
     	return($rootScope.results);*/
     };
 }]);
-}).call(this,require("rH1JPG"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_d25bacf6.js","/")
+}).call(this,require("rH1JPG"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_88d6f910.js","/")
 },{"buffer":3,"rH1JPG":5}],2:[function(require,module,exports){
 (function (process,global,Buffer,__argument0,__argument1,__argument2,__argument3,__filename,__dirname){
 var lookup = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
