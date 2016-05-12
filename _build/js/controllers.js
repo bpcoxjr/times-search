6c029b0d3320a2dc597d29775f88298a911c11f7
Bruce P Cox Jr <bpcoxjr@yahoo.com>
2016-05-11 21:41:36 -0400

    more media query work

--- _build/js/controllers.js
+++ _build/js/controllers.js
@@ -98,7 +98,7 @@ angular.module('timesSearchApp')
     };
 }]);
 
-}).call(this,require("rH1JPG"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_3b76478b.js","/")
+}).call(this,require("rH1JPG"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_56d92558.js","/")
 },{"buffer":3,"rH1JPG":5}],2:[function(require,module,exports){
 (function (process,global,Buffer,__argument0,__argument1,__argument2,__argument3,__filename,__dirname){
 var lookup = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
