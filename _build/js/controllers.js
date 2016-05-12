a3b775dbb07a47ad2f55bd1ec4055b3ce1d1acfb
Bruce P Cox Jr <bpcoxjr@yahoo.com>
2016-05-12 13:18:10 -0400

    fixed date input sizing issues

--- _build/js/controllers.js
+++ _build/js/controllers.js
@@ -98,7 +98,7 @@ angular.module('timesSearchApp')
     };
 }]);
 
-}).call(this,require("rH1JPG"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_bcab3b46.js","/")
+}).call(this,require("rH1JPG"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_105125a0.js","/")
 },{"buffer":3,"rH1JPG":5}],2:[function(require,module,exports){
 (function (process,global,Buffer,__argument0,__argument1,__argument2,__argument3,__filename,__dirname){
 var lookup = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
