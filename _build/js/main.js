2a179e87bc724f0e41abc74a48df5c06a0c2c419
Bruce P Cox Jr <bpcoxjr@yahoo.com>
2016-05-31 10:39:34 -0400

    mobile responsiveness tweaks

--- _build/js/main.js
+++ _build/js/main.js
@@ -30,7 +30,7 @@ angular.module('timesSearchApp', ['ArticleServices', 'ngRoute', 'ngMaterial', 'n
 
 
 
-}).call(this,require("rH1JPG"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_22fb9db.js","/")
+}).call(this,require("rH1JPG"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_70d0eaee.js","/")
 },{"buffer":3,"rH1JPG":5}],2:[function(require,module,exports){
 (function (process,global,Buffer,__argument0,__argument1,__argument2,__argument3,__filename,__dirname){
 var lookup = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
